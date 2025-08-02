e0195cc0d5b0ce94740667e385e5ee40b5c90e3c
IAMDAGOOAT <gopinathanaar@gmail.com>
2025-08-02 15:02:37 +0800

    class

---
+++ class.py
@@ -0,0 +1,19 @@
+class fruit:
+    def __init__(self,name,color):
+        self.name = name
+        self.color = color
+        
+apple = fruit('Apple','Red')
+
+print(apple.name)
+print(apple.color)
+
+class student:
+    def __init__(self,name,grade):
+        self.name = name
+        self.grade = grade
+        
+hi=student('Aaron','6')
+        
+print(hi.name)
+print(hi.grade)
